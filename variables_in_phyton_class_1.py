bc5bbe27c898dd318dfb4770506a1664cdafaaaf
muhammedfasanya <muhammedofasanya@gmail.com>
2026-04-22 17:31:11 -0230

    Add files via upload

---
+++ variables_in_phyton_class_1.py
@@ -0,0 +1,41 @@
+
+# Variables vary. They can change. They make our lives easier.
+
+# I like to think of variables as containers used to store specific objects. The objects are
+# known as data, and the variables holds data.
+
+# Variable declaration
+
+first_name = "dave"  #text string
+
+last_name = "wave"  #text string
+
+age = 10  #number int
+
+score = 4.7  #float decimal
+
+developer = True  #boolean
+
+question = 60 > 10
+
+# calling the variables
+
+print(question)
+
+print(score + 4)
+
+# AUGMENTED ASSIGNMENT OPERATOR
+
+word = "sports"
+
+word = word + " car"
+
+print(word)
+
+word = "bwave"
+
+word += "ict"
+
+print(word)
+
+
